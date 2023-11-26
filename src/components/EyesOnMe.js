e306d7e6b769bf25bece00a354af4ddedcc505ee
alex <alex.musyoki@student.moringaschool.com>
2023-11-26 16:23:33 +0300

    Completed assignments

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+let EyesOnMe = () => {
+    let handleFocus = () => {
+        console.log('Good!');
+    }
+
+    let hanldleBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button onFocus={handleFocus} onBlur={hanldleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
